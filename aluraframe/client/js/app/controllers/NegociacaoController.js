e1217c15ea37a22d39fe6d4b901b8f141eb34245
alairfonseca <alairfonseca13@gmail.com>
2017-05-08 23:25:10 -0300

    fim do segundo capitulo

---
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,42 @@
+class NegociacaoController {
+
+  constructor() {
+    let $ = document.querySelector.bind(document);
+
+    this._inputData = $('#data');
+    this._inputQuantidade = $('#quantidade');
+    this._inputValor = $('#valor');
+  }
+
+   adiciona(event) {
+     event.preventDefault();
+
+    //  let data = new Date(
+    //    ...this._inputData.value.split('-')
+    //    .map(function(item, index) {
+    //      if (index == 2) {
+    //        return item - 1;
+    //      }
+    //      return item;
+    //    })
+    //  );
+
+    //  let data = new Date(
+    //    ...this._inputData.value.split('-')
+    //    .map((item, index) => return item - index % 2)
+    //  );
+    let data = new Date(this._inputData.value.split('-'));
+
+     let negociacao = new Negociacao(
+       data,
+       this._inputQuantidade.value,
+       this._inputValor.value
+     );
+
+     console.log(negociacao);
+    //  console.log(this.inputData.value);
+    //  console.log(this.inputQuantidade.value);
+    //  console.log(this.inputValor.value);
+   }
+
+}
