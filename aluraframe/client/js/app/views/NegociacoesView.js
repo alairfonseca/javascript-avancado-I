a3a7c608595b0b46d5ad85f3ad41d8a0dc0b4ae6
alairfonseca <alairfonseca13@gmail.com>
2017-05-11 19:49:18 -0300

    finalizando o capitulo

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -17,18 +17,24 @@ class NegociacoesView {
         </thead>
 
         <tbody>
-        </tbody>
-          ${negociacoes.negociacoes.map(n => {
-            return `
+          ${negociacoes.negociacoes.map(n =>
+             `
               <tr>
                 <td>${DateHelper.dataParaTexto(n.data)}</td>
                 <td>${n.quantidade}</td>
                 <td>${n.valor}</td>
                 <td>${n.volume}</td>
               </tr>
-            `;
-          }).join('')}
+            `
+          ).join('')}
+          </tbody>
         <tfoot>
+          <td colspan="3"></td>
+          <td>
+            ${negociacoes.negociacoes.reduce(
+              (total, n) => total + n.volume , 0.0)
+            }
+          </td>
         </tfoot>
     `;
   }
