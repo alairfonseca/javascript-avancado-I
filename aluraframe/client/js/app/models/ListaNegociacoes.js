c690b8c0d11ec9e46f2c327bb661cea80d0cd112
alairfonseca <alairfonseca13@gmail.com>
2017-05-14 14:18:10 -0300

    iniciando terceiro modulo

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,22 +1,36 @@
 class ListaNegociacoes {
 
-  constructor() {
-    this._negociacoes = [];
-  }
+    constructor() {
+        
+        this._negociacoes = [];
+    }
 
-  adiciona(negociacao) {
-    this._negociacoes = [].concat(this._negociacoes, negociacao);
-    //this._negociacoes.push(negociacao);
-  }
+    adiciona(negociacao) {
+        
+        this._negociacoes.push(negociacao);
+    }
 
-  get negociacoes() {
-    // retornando uma nova lista de negociacoes para que
-    // a lista original nao possa ser modificada.
-    return [].concat(this._negociacoes);
-  }
+    get negociacoes() {
+        
+        return [].concat(this._negociacoes);
+    }
 
-  esvazia() {
-    this._negociacoes = [];
-  }
+    esvazia() {
+        
+        this._negociacoes = [];
+    }
+    
+    get volumeTotal() {
+       return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
+    }
+    
+    ordena(criterio) {
 
+        this._negociacoes.sort(criterio);        
+    }
+    
+    inverteOrdem() {
+
+        this._negociacoes.reverse();
+    }    
 }
--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,31 +1,30 @@
 class Negociacao {
-
-  constructor(data, quantidade, valor) {
-    // cria um novo objeto do tipo Date para que seja
-    //armazenada uma referencia segura.
-    this._data = new Date(data.getTime());
-    this._quantidade = quantidade;
-    this._valor = valor;
-
-    // congela o objeto para que os atributos sejam imutaveis.
-    Object.freeze(this);
-  }
-
-  get volume() {
-    return this._quantidade * this._valor;
-  }
-
-  get data() {
-    // retornando uma copia da data, pois o objeto Date pode ser mutado.
-    return new Date(this._data.getTime());
-  }
-
-  get quantidade() {
-    return this._quantidade;
-  }
-
-  get valor() {
-    return this._valor;
-  }
-
+    
+    constructor(data, quantidade, valor) {
+        
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+    
+    get volume() {
+        
+        return this._quantidade * this._valor;
+    }
+    
+    get data() {
+        
+        return new Date(this._data.getTime());
+    }
+    
+    get quantidade() {
+        
+        return this._quantidade;
+    }
+    
+    get valor() {
+        
+        return this._valor;
+    }
 }
